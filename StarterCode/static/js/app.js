0fd459187c9d56ad3263afd02f0367d50bc58a40
Bobby Benedict <91276925+robertfben@users.noreply.github.com>
2022-09-18 14:19:56 -0700

    9-18-22

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -40,6 +40,7 @@
 //     });
 //   }
 
+// function to grab the data and append it to its respective list for charts
 function init(data){
     console.log(data);
     var sample_values = []
@@ -50,6 +51,7 @@ function init(data){
     for (let i = 0; i < data.samples.length; i++) {
         //console.log(data.samples[i].id)
         sample_ids.push(data.samples[i].id)
+        //console.log(sample_ids)
         otu_ids = data.samples[i].otu_ids
         //console.log(otu_ids[0])
         otu_labels = data.samples[i].otu_labels
@@ -61,43 +63,60 @@ function init(data){
 // how do i create google chart for top 10 otu ids? --< google
 // try to populate dropdown list first before charts
 
+// On change to the DOM, call getData()
+d3.selectAll("#selDataset").on("change", Dropdown);
+
+// Function called when dropdown menu item is selected
+function Dropdown() {
+    let dropdownMenu = d3.select("#selDataset");
+    // Assign the value of the dropdown menu option to a letiable
+    let dataset = dropdownMenu.property("value");
+    // Initialize an empty array for the country's data
+    //data = []
+    
+    sample_ids.forEach(sample => {
+        dropdownMenu
+            .append('option')
+            .text(sample)
+            .property('value', sample);
+    });
+};
+
+////////////////////////////////////////////////////////
+// trace for bar chart
+var barChart = {
+    type: 'bar',
+    x: sample_values,
+    y: `OTU ${otu_ids}`,
+    hovertext: otu_labels,
+    orientation: 'h'
+};
+
+// Data trace array
+let barTraceData = [barChart];
+
+// Render the plot to the div tag with id "bar"
+Plotly.newPlot("bar", barTraceData);
+
+////////////////////////////////////////////////////////
+
+// trace for bubble chart
+var bubbleChart = {
+    x: otu_ids,
+    y: sample_values,
+    text: otu_labels,
+    mode: 'markers',
+    marker: {
+        size: sample_values,
+        color: otu_ids
+    }
+};
+
+// Data trace array
+let bubbleTraceData = [bubbleChart];
+
+// Render the plot to the div tag with id "bubble"
+Plotly.newPlot("bubble", bubbleTraceData);
+
+
 d3.json('./samples.json').then(data => init(data))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//     console.log(data)
-//     var sample_values = data['sample_values']
-//     console.log(sample_values)
-//     var otu_ids = data['otu_ids']
-//     var otu_labels = data['otu_labels']
-
-//     // create trace for bar chart
-//     var trace  = {
-//         type: 'bar',
-//         x: sample_values,
-//         y: otu_ids,
-//         hovertext: otu_labels,
-        
-//     }
-
-//     // assign trace to an array
-//     var data = [trace]
-
-//     var layout = {
-//         title: 'Top Ten Bacteria By Volume'
-//     }
-// })
-
