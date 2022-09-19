e5a1bcabd85c78880b5599971e3c3b96338a3637
Bobby Benedict <91276925+robertfben@users.noreply.github.com>
2022-09-19 15:19:51 -0700

    9-19-22

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -47,7 +47,7 @@
 // });
 
 // Call init() when a change takes place to the DOM
-d3.selectAll("#selDataset").on("change", init);
+//d3.selectAll("#selDataset").on("change", init);
 
 function init() {
        // Grab a reference to the dropdown select element
@@ -66,16 +66,16 @@ function init() {
      
          // Use the first sample from the list to build the initial plots
          var firstSample = sampleNames[0];
-         buildCharts(firstSample);
-         buildMetadata(firstSample);
+         buildCharts(firstSample);
+         buildMetadata(firstSample);
        });
      }
      
-     // Initialize the dashboard
-     init();
+     
    
      function optionChanged(newSample) {
        // Fetch new data each time a new sample is selected
+       buildCharts(newSample);
        buildMetadata(newSample);
        
      };
@@ -102,99 +102,108 @@ function init() {
          });
      
        });
-     }
 
+    // update the charts when a new id is selected from the dropdown
+    // Plotly.restyle('bar', )
+      };
 
-// function for bar chart
-d3.json('./samples.json').then((data) => {
-    // create variable that holds the samples array
-    var samples = data.samples;
+// buildCharts function
+function buildCharts(sample){
+    // function for bar chart
+    d3.json('./samples.json').then((data) => {
+        // create variable that holds the samples array
+        var samples = data.samples;
 
-    // variable to hold first sample in array
-    var result = samples[0];
-    //console.log(result)
+        // variable to hold first sample in array
+        var resultArray = samples.filter(obj => obj.id == sample);
+        // 
+        var result = resultArray[0];
 
-    // variables holding the necessary data arrays
-    var otu_ids = result.otu_ids;
-    var otu_ids = otu_ids.slice(0, 10);
-    console.log(otu_ids)
-    var otu_labels = result.otu_labels;
-    var sample_values = result.sample_values;
-    
-    // filter only to top 10 otu_ids and add OTU to string label
-    var yticks = otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
-//     // Create the trace for the bar chart. 
-//     var barData = [
-//       {
-//         y: yticks,
-//         x: sample_values.slice(0, 10).reverse(),
-//         text: otu_labels.slice(0, 10).reverse(),
-//         type: "bar",
-//         orientation: "h",
-//       }
-//     ];
+        // variables holding the necessary data arrays
+        var otu_ids = result.otu_ids;
+        var otu_ids = otu_ids.slice(0, 10);
+        console.log(otu_ids)
+        var otu_labels = result.otu_labels;
+        var sample_values = result.sample_values;
+        
+        // filter only to top 10 otu_ids and add OTU to string label
+        var yticks = otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
+    //     // Create the trace for the bar chart. 
+    //     var barData = [
+    //       {
+    //         y: yticks,
+    //         x: sample_values.slice(0, 10).reverse(),
+    //         text: otu_labels.slice(0, 10).reverse(),
+    //         type: "bar",
+    //         orientation: "h",
+    //       }
+    //     ];
 
-    // bar chart trace
-    var barChart = {
-        x: sample_values.slice(0, 10).reverse(),
-        y: yticks,
-        type: 'bar',
-        text: otu_labels,
-        orientation: 'h'
-    };
+        // bar chart trace
+        var barChart = {
+            x: sample_values.slice(0, 10).reverse(),
+            y: yticks,
+            type: 'bar',
+            text: otu_labels,
+            orientation: 'h'
+        };
 
-    // Data array
-    var barChartTrace = [barChart];
+        // Data array
+        var barChartTrace = [barChart];
 
-    // render plot
-    Plotly.newPlot('bar', barChartTrace);
+        // render plot
+        Plotly.newPlot('bar', barChartTrace);
 
-    Plotly.restyle('bar', data)
+        Plotly.restyle('bar', data)
 
-})
+    })
 
-// function for bubble chart
-d3.json('./samples.json').then((data) => {
-    // create variable that holds the samples array
-    var samples = data.samples;
+    // function for bubble chart
+    d3.json('./samples.json').then((data) => {
+        // create variable that holds the samples array
+        var samples = data.samples;
 
-    // variable to hold first sample in array
-    var result = samples[0];
-    //console.log(result)
+        // variable to hold first sample in array
+        var result = samples[0];
+        //console.log(result)
 
-    // variables holding the necessary data arrays
-    var otu_ids = result.otu_ids;
-    var otu_ids = otu_ids;
-    var otu_labels = result.otu_labels;
-    var sample_values = result.sample_values;
-    
-    // bubble chart trace
-    var bubbleChart = {
-        x: otu_ids,
-        y: sample_values,
-        text: otu_labels,
-        mode: 'markers',
-        marker: {
-            size: sample_values,
-            color: otu_ids
+        // variables holding the necessary data arrays
+        var otu_ids = result.otu_ids;
+        var otu_ids = otu_ids;
+        var otu_labels = result.otu_labels;
+        var sample_values = result.sample_values;
+        
+        // bubble chart trace
+        var bubbleChart = {
+            x: otu_ids,
+            y: sample_values,
+            text: otu_labels,
+            mode: 'markers',
+            marker: {
+                size: sample_values,
+                color: otu_ids
+                }
+
+        };
+
+        // Apply xaxis title to layout
+        var layout = {
+            xaxis: {
+                title: 'OTU ID'
             }
+        }
 
+        // Data array
+        var bubbleChartTrace = [bubbleChart];
+
+        // render plot
+        Plotly.newPlot('bubble', bubbleChartTrace, layout);
+
+    })
     };
 
-    // Apply xaxis title to layout
-    var layout = {
-        xaxis: {
-            title: 'OTU ID'
-        }
-    }
-
-    // Data array
-    var bubbleChartTrace = [bubbleChart];
-
-    // render plot
-    Plotly.newPlot('bubble', bubbleChartTrace, layout);
-
-})
+// Initialize the dashboard
+ init();
 
 // var sample_values = []
 // var otu_ids = []
