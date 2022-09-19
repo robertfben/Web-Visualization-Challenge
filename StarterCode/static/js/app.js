c42e41ca43f6ee271fe79bf502a3f83b1153b5e8
Bobby Benedict <91276925+robertfben@users.noreply.github.com>
2022-09-19 14:15:08 -0700

    Update app.js

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -16,36 +16,93 @@
 //     });
 //   }
 
-
 // function for dropdown
-d3.json('./samples.json').then((data) => {
+// d3.json('./samples.json').then((data) => {
 
-    // create variable that holds the samples array
-    var samples = data.samples;
+//     // create variable that holds the samples array
+//     var samples = data.samples;
 
-    // variable to hold first sample in array
-    var result = samples;
-    //console.log(result)
+//     // variable to hold first sample in array
+//     var result = samples;
+//     //console.log(result)
 
-    // variables holding the necessary data arrays
-    var otu_ids = result.otu_ids;
-    var otu_ids = otu_ids;
-    //console.log(otu_ids)
-    var otu_labels = result.otu_labels;
-    var sample_values = result.sample_values;
+//     // variables holding the necessary data arrays
+//     var otu_ids = result.otu_ids;
+//     var otu_ids = otu_ids;
+//     //console.log(otu_ids)
+//     var otu_labels = result.otu_labels;
+//     var sample_values = result.sample_values;
 
-    // use d3 to select dropdown menu
-    var dropdownMenu = d3.select('#selDataset');
-    // Assign value of dropdown menu option to variable
-    var dataset = dropdownMenu.property('value');
+//     // use d3 to select dropdown menu
+//     var dropdownMenu = d3.select('#selDataset');
+//     // Assign value of dropdown menu option to variable
+//     var dataset = dropdownMenu.property('value');
 
-    otu_ids.forEach(sample => {
-        dropdownMenu
-            .append('option')
-            .text(sample)
-            .property('value', sample);
-    });
-});
+//     otu_ids.forEach(sample => {
+//         dropdownMenu
+//             .append('option')
+//             .text(sample)
+//             .property('value', sample);
+//     });
+// });
+
+// Call init() when a change takes place to the DOM
+d3.selectAll("#selDataset").on("change", init);
+
+function init() {
+       // Grab a reference to the dropdown select element
+       var selector = d3.select("#selDataset");
+     
+       // Use the list of sample names to populate the select options
+       d3.json("samples.json").then((data) => {
+         var sampleNames = data.names;
+     
+         sampleNames.forEach((sample) => {
+           selector
+             .append("option")
+             .text(sample)
+             .property("value", sample);
+         });
+     
+         // Use the first sample from the list to build the initial plots
+         var firstSample = sampleNames[0];
+         buildCharts(firstSample);
+         buildMetadata(firstSample);
+       });
+     }
+     
+     // Initialize the dashboard
+     init();
+   
+     function optionChanged(newSample) {
+       // Fetch new data each time a new sample is selected
+       buildMetadata(newSample);
+       
+     };
+   
+     // Demographics Panel 
+   function buildMetadata(sample) {
+       d3.json("samples.json").then((data) => {
+         var metadata = data.metadata;
+         // Filter the data for the object with the desired sample number
+         var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+         var result = resultArray[0];
+        
+         // Use d3 to select the panel with id of `#sample-metadata`
+         var PANEL = d3.select("#sample-metadata");
+     
+         // Use `.html("") to clear any existing metadata
+         PANEL.html("");
+     
+         // Use `Object.entries` to add each key and value pair to the panel
+         // Hint: Inside the loop, you will need to use d3 to append new
+         // tags for each key-value in the metadata.
+         Object.entries(result).forEach(([key, value]) => {
+           PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
+         });
+     
+       });
+     }
 
 
 // function for bar chart
@@ -64,12 +121,25 @@ d3.json('./samples.json').then((data) => {
     var otu_labels = result.otu_labels;
     var sample_values = result.sample_values;
     
+    // filter only to top 10 otu_ids and add OTU to string label
+    var yticks = otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
+//     // Create the trace for the bar chart. 
+//     var barData = [
+//       {
+//         y: yticks,
+//         x: sample_values.slice(0, 10).reverse(),
+//         text: otu_labels.slice(0, 10).reverse(),
+//         type: "bar",
+//         orientation: "h",
+//       }
+//     ];
+
     // bar chart trace
     var barChart = {
-        x: sample_values.reverse(),
-        y: `OTU ${otu_ids}`,
+        x: sample_values.slice(0, 10).reverse(),
+        y: yticks,
         type: 'bar',
-        hovertext: otu_labels,
+        text: otu_labels,
         orientation: 'h'
     };
 
@@ -78,6 +148,8 @@ d3.json('./samples.json').then((data) => {
 
     // render plot
     Plotly.newPlot('bar', barChartTrace);
+
+    Plotly.restyle('bar', data)
 
 })
 
@@ -109,11 +181,18 @@ d3.json('./samples.json').then((data) => {
 
     };
 
+    // Apply xaxis title to layout
+    var layout = {
+        xaxis: {
+            title: 'OTU ID'
+        }
+    }
+
     // Data array
     var bubbleChartTrace = [bubbleChart];
 
     // render plot
-    Plotly.newPlot('bubble', bubbleChartTrace);
+    Plotly.newPlot('bubble', bubbleChartTrace, layout);
 
 })
 
